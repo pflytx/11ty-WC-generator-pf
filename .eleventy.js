c7ffcc1d3bd05fb281d4b89e85a8d838d27e44f8
pflytx <phillip.faniol@lytx.com>
2021-10-25 21:01:16 -0700

    trying nav plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
   // Optional, used for the main application styles
   eleventyConfig.addWatchTarget("./src/app.css");
   eleventyConfig.addPassthroughCopy("./src/app.css");
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   // Allows transforming ex. `hello-world` into `HelloWorld`
   // for the component `class` export
@@ -10,6 +13,11 @@ module.exports = function (eleventyConfig) {
       return;
     }
     return str.split('-').map((part) => part.charAt(0).toUpperCase() + part.slice(1)).join('');
+  });
+
+  //adding npm package for use
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@lit": "assets/lit",
   });
 
   return {
